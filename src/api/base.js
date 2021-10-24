9901a53dc291f78f846de8c8a2732791989b536a
Riccardo Busetti <riccardob36@gmail.com>
2021-10-24 12:10:04 +0200

    Remove favicon

--- src/api/base.js
+++ src/api/base.js
@@ -1,3 +1,3 @@
-export const BASE_URL = `http://elencho-scraper.herokuapp.com`;
+export const BASE_URL = `https://elencho-scraper.herokuapp.com`;
 
 export const AVAILABILITY_ENDPOINT = `${BASE_URL}/availability`;
